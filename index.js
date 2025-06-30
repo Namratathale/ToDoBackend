f2d9bde810565234178d06b042211095c0c7dcd2
Namratathale <157197502+Namratathale@users.noreply.github.com>
2025-06-30 16:27:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ app.use(cors(
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGOURL)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
+mongoose.connect(MONGOURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
 const userSchema = new mongoose.Schema({
   username: String,
